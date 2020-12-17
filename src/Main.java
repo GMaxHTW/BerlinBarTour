f784bdc457b9a063202943c511ba9cc87a312dfa
GMaxHTW <72798750+GMaxHTW@users.noreply.github.com>
2020-12-17 16:03:06 +0100

    Objet tourGuide hinzugefügt Unnötige Methoden rausgeschmissen

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,18 @@ public class Main {
         Student student5 = new Student("Brandon", "Drew", "07.07.1994");
         Student nikolaus = new Student("Nikolaus", "Der Knecht vom Weihnachtsmann", "24.12.245");
 
-        // Methode introduce yourself --> Stellt sich Objekt Student vor
-        student1.introduceYourself();
-        student2.introduceYourself();
+        TourGuide tourGuide1 = new TourGuide("Peter", "Lustig", "02.03.1985");
+
+        tourGuide1.introduceYourself();
+
+        tourGuide1.addStudentToCrew(student1);
+//        tourGuide1.addStudentToCrew(student2);
+//        tourGuide1.addStudentToCrew(student3);
+//        tourGuide1.addStudentToCrew(student4);
 
 
-        student1.drinkBeer();
-        student2.drinkBeer();
+
+
 
 
 
