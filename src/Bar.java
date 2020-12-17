e37755376ee87b9a3db249b056d143da5a5b96c4
GMaxHTW <72798750+GMaxHTW@users.noreply.github.com>
2020-12-17 12:47:08 +0100

    Attribute der Bar (I) name, (II) tips
    Array mit Namen verschiedener Bars
    
    Konstruktor Bar (bis jetzt nut Name)
    
    Methoden:
    (I) getRandomBarName, (II) createRandomBar

--- src/Bar.java
+++ src/Bar.java
@@ -3,4 +3,26 @@ public class Bar {
     /**
      * Zählt Bewertungen hoch
      */
+    private String name;
+    private double tips = 0;
+
+    private static String [] barName = new String[] {"Hansbachbar", "Urban Spree Bar", "Klunkerkranich", "minimal Bar", "Radio Label Bar"};
+
+    public Bar (String name) {
+        this.name = name;
+    }
+
+
+    public static String getRandomBarName () {
+        return barName[(int) (5 * Math.random())];
+    }
+
+
+    public Bar createRandomBar () {
+        String name = getRandomBarName();
+        System.out.println("Cool heute geht es also in die " + name +".\nNa dann los geht's");
+        return new Bar (name);
+    }
+
+
 }
