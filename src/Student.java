f317a1d9b4afa91fc5f3fa74351e06e40648c07a
GMaxHTW <72798750+GMaxHTW@users.noreply.github.com>
2020-12-17 13:16:23 +0100

    Neue Methode (I) Go to Random bar
    Zusätzliche wurden unnötige Methoden gelöscht

--- src/Student.java
+++ src/Student.java
@@ -16,10 +16,15 @@ public class Student {
     private final String firstName;
     private final String dateOfBirth;
 
+
     // Veränderbare Attribute des Studenten
     // private Adress adress;
     private double money = 50;
     private boolean drunk = false;
+    private boolean broke = false;
+
+    Bar Bar;
+
 
     // Konstrukor --> Vorlage zum Erzeugen von nem Objekt Student
     public Student (String firstName, String lastName, String dateOfBirth) {
@@ -30,69 +35,14 @@ public class Student {
 
 
     public void introduceYourself () {
-        System.out.println("\nHallo ich bin der " + firstName
+        System.out.println("\nHallo ich bin " + firstName
                 + "\nFreut mich euch kennen zu lernnen");
     }
 
 
-    /**
-     * Die Methode earnMoney
-     * Zufallszahl zwischen
-     */
-    public void earnMoney () {
-        System.out.println(firstName + " geht arbeiten im Berghain");
-        money = money + 40;
+    public void goToRandomBar() {
+        this.Bar = Bar.createRandomBar();
     }
 
-    /**
-     * Methode zum senden von Geld
-     *
-     * Beispiel student --> sendet Geld an studentReceivingMoney
-     * @param studentReceivingMoney
-     */
-    public void sendMoney (int moneyToSend, Student studentReceivingMoney) {
-        money = money - moneyToSend;
-        studentReceivingMoney.money = studentReceivingMoney.money + moneyToSend;
-        System.out.println("\n" + this.firstName + "Neuer Geldbestand " + this.money +
-                "\n" + studentReceivingMoney.firstName + " Neuer Geldbestand " + studentReceivingMoney.money);
-    }
-
-
-
-    /**
-     * Der Student geht mit seinen Freunden Party machen
-     * In der Location trinkt man auf jeden Fall erstmal ein Bier oder einer Wasser
-     * Jeder so wie er das will aber das kostet dann erstmal 2,50 €
-     *
-     * Mit einer Wahrscheinlichkeit von 40% holt man sich dire
-     * */
-    public void drinkBeer () {
-        double beerPrice = 2.5;
-        money = money - beerPrice;
-        System.out.println("\n" + firstName + " trinkt genüsslich sein Bier" +
-                "\nMomentaner Kassenschnitt " + money  +"€");
-    }
-
-    public void drinkShot () {
-        double beerPrice = 2.5;
-        money = money - beerPrice;
-        System.out.println("\n" + firstName + " trinkt genüsslich sein Bier" +
-                "\nMomentaner Kassenschnitt " + money  +"€");
-    }
-
-    public void drinkWater () {
-        double beerPrice = 2.5;
-        money = money - beerPrice;
-        System.out.println("\n" + firstName + " trinkt genüsslich sein Bier" +
-                "\nMomentaner Kassenschnitt " + money  +"€");
-    }
-
-    public void chooseRandomBar() {
-
-    }
-
-    public void chooseRandomClub () {
-
-    }
 
 }
